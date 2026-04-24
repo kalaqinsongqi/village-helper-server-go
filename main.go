9c37ef45eb5bdc46338e51407b9ff13ba19244e7
songqi <songqi@example.com>
2026-04-24 14:09:54 +0800

    fix: disable AllowCredentials in CORS to fix Electron file:// requests

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 	}))
 
 	// 路由
